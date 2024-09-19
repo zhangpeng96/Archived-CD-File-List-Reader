159250a7f0a62b45eb4c197807396300e850a88c
ZhangPeng <sjiaomail@gmail.com>
2024-09-20 00:13:05 +0800

    Update: add default video engine configuration

--- main.py
+++ main.py
@@ -11,8 +11,11 @@ import cv2
 
 # DISK_PATH = 'D:\Live\studio'
 # DISK_PATH = 'E:'
-print('视频档案光盘基本信息读取 by zhangpeng96')
-print('2024/9/25\n\n')
+print('=======================================')
+print('==      视频档案光盘基本信息读取      ==')
+print('==                  v0.1.2 2024/9/14 ==')
+print('==                    by zhangpeng96 ==')
+print('=======================================')
 
 while True:
 
@@ -22,14 +25,13 @@ while True:
     TOTAL_SIZE = 0
     TOTAL_COUNT = 0
     TOTAL_DURATION = 0
+    IS_VIDEO_DISC = False
+    ENGINE = config['video']['default_engine']
     MEDIA_EXT = ['AAC','MP3','WAV','MP4','TS','MTS','WMV','AVI','MPEG','MPG', 'FLV', 'MKV','MOV','MPE','NSR','NSV','VOB','WMP', 'M4A','OGG']
     trees = []
 
-
+    print('正在读取文件中，文件列表如下：\n')
     for root, dirs, files in walk(DISK_PATH):
-        # TOTAL_SIZE = sum(path.getsize( path.join(root,file) for file in files))
-        # print(TOTAL_SIZE)
-        print('正在读取文件中，文件列表如下：\n')
         for file in files:
             status = stat( path.join(root, file) )
             TOTAL_SIZE += status.st_size
@@ -39,17 +41,33 @@ while True:
             print('{}'.format(filename))
             # print(status.st_size)
             if ext in MEDIA_EXT:
-                print('> 读取视频时长中，请稍等...')
-                try:
-                    video = VideoFileClip(path.join(root, file))
-                except:
-                    print('> FFmpeg解析时长失败，换用OpenCV重新解析...')
-                    video = cv2.VideoCapture(path.join(root, file))
-                    fps = video.get(cv2.CAP_PROP_FPS)
-                    totalNoFrames = video.get(cv2.CAP_PROP_FRAME_COUNT)
-                    duration = totalNoFrames // fps
+                print('└─ 读取视频时长中，请稍等...')
+                if ENGINE == 'ffmpeg':
+                    try:
+                        video = VideoFileClip(path.join(root, file))
+                    except:
+                        print('└─ FFmpeg读取失败，换用OpenCV读取...')
+                        video = cv2.VideoCapture(path.join(root, file))
+                        fps = video.get(cv2.CAP_PROP_FPS)
+                        totalNoFrames = video.get(cv2.CAP_PROP_FRAME_COUNT)
+                        duration = totalNoFrames // fps
+                        IS_VIDEO_DISC = True
+                    else:
+                        duration = video.duration
+                    print('└─ 读取成功，时长为：{}'.format(strftime("%H:%M:%S",gmtime(duration))))
                 else:
-                    duration = video.duration
+                    try:
+                        video = cv2.VideoCapture(path.join(root, file))
+                        fps = video.get(cv2.CAP_PROP_FPS)
+                        totalNoFrames = video.get(cv2.CAP_PROP_FRAME_COUNT)
+                    except:
+                        print('└─ OpenCV读取失败，换用FFmpeg读取...')
+                        video = VideoFileClip(path.join(root, file))
+                        duration = video.duration
+                        IS_VIDEO_DISC = True
+                    else:
+                        duration = totalNoFrames // fps
+                    print('└─ 读取成功，时长为：{}'.format(strftime("%H:%M:%S",gmtime(duration))))
                 TOTAL_DURATION += duration
             else:
                 duration = 0
@@ -62,10 +80,7 @@ while True:
                 'mtime': status.st_mtime
             })
 
-
-    # pprint(trees)
-
-    print('========== 光盘文件统计 ==========')
+    print('\n========== 光盘文件统计 ==========')
     type_count = Counter(map(lambda x: x['ext'], trees))
     type_count = list(sorted(type_count.items(), key=lambda x:-x[1]))
     mtime = max(map(lambda x:x['mtime'], trees))
@@ -91,8 +106,8 @@ while True:
     print('视频总时长：\t{}'.format(strftime("%H:%M:%S",gmtime(TOTAL_DURATION))))
     # print(mtime)
 
-    print('========= 以下信息已复制 =========')
-    # type_count_slim = type_count.items()
+    print('\n========= 以下信息已复制 =========')
+
     if 0 < TOTAL_DURATION < 3600:
         read_duration = strftime("%M:%S", gmtime(TOTAL_DURATION))
     elif TOTAL_DURATION >= 3600:
@@ -113,11 +128,11 @@ while True:
         cliptext = autocopy.format(**clipDict)
     except:
         print('输出信息格式配置有误，已使用缺省模式')
-        cliptext = '{year}\t{date}\t{size}\t{duration}\t{count}\t{types}'.format(**clipDict).encode('utf-8').decode('unicode_escape')
+        cliptext = '{year}\t{date}\t{size}\t{duration}\t{count}\t{types}'.format(**clipDict)
     else:
         cliptext = autocopy.format(**clipDict).encode('utf-8').decode('unicode_escape')
     print(cliptext)
-    print('=================================')
+    print('==================================')
 
     pyperclip.copy(cliptext)
 
